ac2575196d098d758d27c653a2ff3f8be5c64db2
Derrice <derricewright@gmail.com>
2023-08-23 14:06:19 -0400

    v4

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -22,7 +22,6 @@ class Instructor(models.Model):
     def __str__(self):
         return self.user.username
 
-
 # Learner model
 class Learner(models.Model):
     user = models.ForeignKey(
@@ -51,7 +50,6 @@ class Learner(models.Model):
         return self.user.username + "," + \
                self.occupation
 
-
 # Course model
 class Course(models.Model):
     name = models.CharField(null=False, max_length=30, default='online course')
@@ -67,14 +65,12 @@ class Course(models.Model):
         return "Name: " + self.name + "," + \
                "Description: " + self.description
 
-
 # Lesson model
 class Lesson(models.Model):
     title = models.CharField(max_length=200, default="title")
     order = models.IntegerField(default=0)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     content = models.TextField()
-
 
 # Enrollment model
 # <HINT> Once a user enrolled a class, an enrollment entry should be created between the user and course
@@ -93,7 +89,6 @@ class Enrollment(models.Model):
     date_enrolled = models.DateField(default=now)
     mode = models.CharField(max_length=5, choices=COURSE_MODES, default=AUDIT)
     rating = models.FloatField(default=5.0)
-
 
 # <HINT> Create a Question Model with:
     # Used to persist question content for a course
